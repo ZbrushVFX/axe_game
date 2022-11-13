15de9930240ee4d500041887487ae3dea3ef21b7
ZbrushVFX <aka.device@gmail.com>
2022-11-13 18:56:00 +0300

    update: final game

--- Axe.cpp
+++ Axe.cpp
@@ -1,11 +1,7 @@
-
 #include "raylib.h"
-
-
 int main()
 {
-
-// Create game window
+// Window dimensions
   int width{800};
   int height{450};
   InitWindow(width, height,"My Window");
@@ -14,17 +10,16 @@ int main()
   int circle_x{200};
   int circle_y{200};
   int circle_radius{25};
-
-// axe coordinates
-  int axe_x{400};
-  int axe_y{0};
-  int axe_lenght{50};
-
 // circle edges
   int l_circle_x{circle_x - circle_radius};
   int r_circle_x{circle_x + circle_radius};
   int u_circle_y{circle_y - circle_radius};
   int b_circle_y{circle_y + circle_radius};
+
+// axe coordinates
+  int axe_x{400};
+  int axe_y{0};
+  int axe_lenght{50};
 
 //axe edges
   int l_axe_x{axe_x};
@@ -32,31 +27,22 @@ int main()
   int u_axe_y{axe_y};
   int b_axe_y{axe_y + axe_lenght};
 
-// Axe collision
-  bool collision_with_axe = 
-                           (b_axe_y >= u_circle_y) && 
-                           (u_axe_y <= b_circle_y) && 
-                           (r_axe_x >= l_circle_x) && 
-                           (l_axe_x <= r_circle_x);
-
-// Axe paths
   int direction{10};
+
+  bool collision_with_axe =
+  (b_axe_y >= u_circle_y) &&
+  (u_axe_y <= b_circle_y) &&
+  (r_axe_x >= l_circle_x) &&
+  (l_axe_x <= r_circle_x);
 
 // Update fps
   SetTargetFPS(60);
-
-
-
-
   while (!WindowShouldClose())
   {
 
 // Обновление холста, чтобы не было мигания при двойной буфере
   BeginDrawing();
   ClearBackground(WHITE);
-
-// Game logic begins
-
   if (collision_with_axe)
   {
     DrawText("Game Over", 400, 200, 20, RED);
@@ -71,14 +57,13 @@ int main()
     l_axe_x = axe_x;
     r_axe_x = axe_x + axe_lenght;
     u_axe_y = axe_y;
-    u_axe_y = axe_y + axe_lenght;
-
+    b_axe_y = axe_y + axe_lenght;
 // update collision_with_axe
   collision_with_axe =
-                        (b_axe_y >= u_circle_y) &&
-                        (u_axe_y <= b_circle_y) &&
-                        (r_axe_x >= l_circle_x) &&
-                        (l_axe_x <= r_circle_x);
+    (b_axe_y >= u_circle_y) &&
+    (u_axe_y <= b_circle_y) &&
+    (r_axe_x >= l_circle_x) &&
+    (l_axe_x <= r_circle_x);
 
   DrawCircle(circle_x, circle_y, circle_radius, BLUE);
   DrawRectangle(axe_x, axe_y, axe_lenght, axe_lenght, RED);
@@ -101,10 +86,8 @@ int main()
   circle_x -= 10;
   }
 
-// Game logic ends
-
   }
-
+// Game logic ends
   EndDrawing();
   }
 
